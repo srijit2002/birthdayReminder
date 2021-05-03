ff49e69c8af0103751efae8eb48d201473c31ec8
Srijit <srijitm906@gmail.com>
2021-05-03 17:43:55 +0530

    Improved responsiveness

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,9 @@
 const Data = [
-    { Name: "Srijit Mondal", Img: "https://images.pexels.com/photos/3483800/pexels-photo-3483800.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "29Apr", Age: 19, Id: 1 },
-    { Name: "Sumon Mondal", Img: "https://images.pexels.com/photos/7571856/pexels-photo-7571856.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "29Apr", Age: 19, Id: 2 },
-    { Name: "Sing Mama", Img: "https://images.pexels.com/photos/7083673/pexels-photo-7083673.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "24Nov", Age: 20, Id: 3 },
-    { Name: "Sumon Sing", Img: "https://images.pexels.com/photos/4449136/pexels-photo-4449136.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "29Apr", Age: 21, Id: 4 },
-    { Name: "Laptop", Img: "https://images.pexels.com/photos/5528835/pexels-photo-5528835.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "29Apr", Age: 8, Id: 5 },
-    { Name: "Coding", Img: "https://images.pexels.com/photos/5386358/pexels-photo-5386358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "29Apr", Age: 1, Id: 6 }
+    { Name: "Srijit Mondal", Img: "https://images.pexels.com/photos/3483800/pexels-photo-3483800.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "3May", Age: 19, Id: 1 },
+    { Name: "Sumon Mondal", Img: "https://images.pexels.com/photos/7571856/pexels-photo-7571856.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "3May", Age: 19, Id: 2 },
+    { Name: "Sing Mama", Img: "https://images.pexels.com/photos/7083673/pexels-photo-7083673.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "3May", Age: 20, Id: 3 },
+    { Name: "Sumon Sing", Img: "https://images.pexels.com/photos/4449136/pexels-photo-4449136.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "3May", Age: 21, Id: 4 },
+    { Name: "Laptop", Img: "https://images.pexels.com/photos/5528835/pexels-photo-5528835.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "3May", Age: 8, Id: 5 },
+    { Name: "Coding", Img: "https://images.pexels.com/photos/5386358/pexels-photo-5386358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", DOB: "3May", Age: 1, Id: 6 }
 ]
 export default Data;
